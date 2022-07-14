4efef8eace6f7f06ee776f0babfc40b5553900a3
Hanumaraddi vaddatti <93374801+HanumaraddiBV@users.noreply.github.com>
2022-07-14 21:58:58 +0530

    updated progress page with fetching data from backend

--- src/Pages/ProgressPage/ProgressPage.js
+++ src/Pages/ProgressPage/ProgressPage.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { Component } from "react";
+import './progress.module.css'
 import { connect } from "react-redux";
 
 class ProgressPage extends Component {
@@ -16,6 +17,7 @@ class ProgressPage extends Component {
       .get(`http://localhost:3001/users/${this.props.userId.id}`)
       .then((res) => {
         let userProgressData = res.data;
+        this.setState({...this.state,userReport: userProgressData})
         console.log("userProgressData:", userProgressData);
         if (userProgressData.report.length > 0) {
           this.setState({ userReport: userProgressData });
@@ -33,7 +35,7 @@ class ProgressPage extends Component {
     return (
       <>
         <div>
-          <h2>
+          <div><h2>
             Hey!, {this.props.userId.name} You can view your progress report or
             you can download in pdf format,click! on below buttons according to
             your choice
@@ -41,7 +43,43 @@ class ProgressPage extends Component {
           <div className={"button-div"}>
             <button onClick={()=> this.setState({...this.state,isView: true})}>View</button>
             <button>Download</button>
-          </div>
+          </div></div>
+          <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Category Name</th>
+              <th>Question</th>
+              <th>Correct Answer</th>
+              <th>Your Answer</th>
+              <th>Total Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.userReport?.report?.map((ele,ind) => (
+              <tr key={ele.ind}>
+                <td>{ele.categoryName}</td>
+                <td>{ele.result.map((res,ind)=>{
+                 return( <div>
+                  <p>{ind+1}.{res.questionDescription}</p>
+                 </div>)
+                })}</td>
+                <td>{ele.result.map((res,ind)=>{
+                 return( <div>
+                  <p>{res.correctAns}</p>
+                 </div>)
+                })}</td>
+                <td>{ele.result.map((res,ind)=>{
+                 return( <div>
+                  <p>{res.selectedOption}</p>
+                 </div>)
+                })}</td>
+                <td>{ele.totalScore}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
         </div>
       </>
     );
